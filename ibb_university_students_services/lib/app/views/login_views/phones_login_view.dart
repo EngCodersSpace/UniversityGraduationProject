81601cc4ddcf79702cd331f7fab3a577807c9bb3
SHEHAB <shshehab02@gmail.com>
2024-01-11 23:57:27 +0300

    create project done

---
+++ ibb_university_students_services/lib/app/views/login_views/phones_login_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class PhoneLoginView extends StatelessWidget {
+  const PhoneLoginView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
