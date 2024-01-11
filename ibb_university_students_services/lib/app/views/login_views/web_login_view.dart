e560abba6a73ca63ed446d184089f97a0103ec12
SHEHAB <shshehab02@gmail.com>
2024-01-11 23:57:27 +0300

    create project done

---
+++ ibb_university_students_services/lib/app/views/login_views/web_login_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class WebLoginView extends StatelessWidget {
+  const WebLoginView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
