be9154b4d0b8b10813ab7843159eb0151e9eb2c6
SHEHAB <shshehab02@gmail.com>
2024-05-29 01:51:45 +0300

    Edting TextFiled component and finsh login view design

--- ibb_university_students_services/lib/app/views/login_view/phones_login_view.dart
+++ ibb_university_students_services/lib/app/views/login_view/phones_login_view.dart
@@ -21,6 +21,10 @@ class PhoneLoginView extends StatelessWidget {
   double? fontScale;
 
   LoginController controller = Get.find();
+  List<PopupMenuItem<String>> menuItems = [
+    PopupMenuItem<String>(value: "en", child: SecText("En")),
+    PopupMenuItem<String>(value: "ar", child: SecText("Ar")),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -33,7 +37,7 @@ class PhoneLoginView extends StatelessWidget {
             SizedBox(
               height: (height! * 0.5),
               width: width,
-              child: Image.asset("assets/images/loginbackimage.jpg",
+              child: Image.asset("assets/images/login_background_1.jpeg",
                   fit: BoxFit.fill),
             ),
             SizedBox(
@@ -45,104 +49,175 @@ class PhoneLoginView extends StatelessWidget {
         Container(
           color: const Color.fromRGBO(0, 191, 255, 0.25),
         ),
-        Column(
-          children: [
-            SizedBox(
-              height: height! * 0.4,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  SizedBox(
-                    height: height! * 0.25,
-                    child: Image.asset("assets/images/ibb_university_logo.png",
-                        fit: BoxFit.fill),
-                  ),
-                  MainText("IBB",
-                      fontWeight: FontWeight.bold,
-                      fontSize: height! * 0.15 / 4),
-                  MainText(
-                    "UNIVERSITY",
-                    fontWeight: FontWeight.bold,
-                    fontSize: height! * 0.15 / 4,
-                  ),
-                ],
+        SizedBox(
+          height: height!,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: height! * 0.02,
               ),
-            ),
-            Container(
-                width: width,
-                height: height! * 0.6,
-                padding: const EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                  color: AppColors.backColor,
-                  borderRadius:
-                      const BorderRadius.vertical(top: Radius.circular(20)),
+              SizedBox(
+                height: height! * 0.25,
+                child: Image.asset("assets/images/ibb_university_logo.png",
+                    fit: BoxFit.fill),
+              ),
+              Container(
+                width: double.maxFinite,
+                decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                  colors: [Colors.transparent, Colors.black87],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                )),
+                child: Column(
+                  children: [
+                    MainText(
+                      "IBB",
+                      fontWeight: FontWeight.bold,
+                      fontSize: height! * 0.15 / 4,
+                    ),
+                    MainText(
+                      "UNIVERSITY",
+                      fontWeight: FontWeight.bold,
+                      fontSize: height! * 0.15 / 4,
+                    ),
+                    SizedBox(
+                      height: height! * 0.05,
+                    )
+                  ],
                 ),
-                child: Form(
-                  key: controller.formKey,
-                  autovalidateMode: AutovalidateMode.onUserInteraction,
-                  child: Column(
-                    children: [
-                      MainText('login'.tr,
-                          fontWeight: FontWeight.bold,
-                          fontSize: height! * 0.15 / 4,
-                          textColor: Colors.black),
-                      SizedBox(
-                        height: height! * 0.6 * 0.1,
-                      ),
-                      CustomTextFormField(
-                        controller: controller.ID,
-                        validator: (id)=>controller.validateID(id),
-                        labelText: 'studentid'.tr,
-                        icon: Icons.account_circle_outlined,
-                      ),
-                      SizedBox(
-                        height: height! * 0.6 * 0.05,
-                      ),
-                      CustomTextFormField(
-                        controller: controller.password,
-                        validator: (pwd)=>controller.validatePassword(pwd),
-                        labelText: 'password'.tr,
-                        icon: Icons.key_sharp,
-                        isPassword: true,
-                      ),
-                      SizedBox(
-                        height: height! * 0.6 * 0.1,
-                      ),
-                      Obx(() => (controller.logging.value)
-                          ? BasicButton(
-                              onPress: (){},
-                              text: 'logging'.tr,
-                              icon:  SizedBox(
-                                height: 25,
-                                width: 25,
-                                child: CircularProgressIndicator(
-                                  color: AppColors.backColor,
-                                ),
+              )
+            ],
+          ),
+        ),
+        Obx(
+          () => Container(
+              width: width,
+              height: height! * controller.heightScale.value,
+              padding: const EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: AppColors.backColor,
+                borderRadius:
+                    const BorderRadius.vertical(top: Radius.circular(20)),
+              ),
+              child: Form(
+                key: controller.formKey,
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        SizedBox(
+                          width: width! * 0.35 * 0.2,
+                        ),
+                        MainText('login'.tr,
+                            fontWeight: FontWeight.bold,
+                            fontSize: height! * 0.15 / 4,
+                            textColor: Colors.black),
+
+                        PopupMenuButton<String>(
+                          initialValue: Get.locale?.languageCode.toString(),
+                          itemBuilder: (BuildContext context) => menuItems,
+                          onSelected: (lang) {
+                            controller.changeLang(lang);
+                          },
+                          child: Row(
+                            children:
+                                (Get.locale?.languageCode.toString() == "en")
+                                    ? [
+                                        const Icon(Icons.language),
+                                        SecText("En"),
+                                      ]
+                                    : [
+                                        SecText("Ar"),
+                                        const Icon(Icons.language),
+                                      ],
+                          ),
+                        ),
+
+                      ],
+                    ),
+                    SizedBox(
+                      height: height! * 0.6 * 0.1,
+                    ),
+                    CustomTextFormField(
+                      controller: controller.id,
+                      validator: (id) => controller.validateID(id),
+                      labelText: 'studentid'.tr,
+                      icon: Icons.account_circle_outlined,
+                      focusNode: controller.idFocus,
+                      onFieldSubmitted: (e) {
+                        controller.idFocus.unfocus();
+                        FocusScope.of(context)
+                            .requestFocus(controller.passwordFocus);
+                      },
+                      onTap: () {
+                        controller.heightScale.value = 0.8;
+                      },
+                      onTapOutside: (e) {
+                        controller.idFocus.unfocus();
+                        controller.heightScale.value = 0.6;
+                      },
+                    ),
+                    SizedBox(
+                      height: height! * 0.6 * 0.05,
+                    ),
+                    CustomTextFormField(
+                      controller: controller.password,
+                      validator: (pwd) => controller.validatePassword(pwd),
+                      labelText: 'password'.tr,
+                      icon: Icons.key_sharp,
+                      isPassword: true,
+                      focusNode: controller.passwordFocus,
+                      onTap: () {
+                        controller.heightScale.value = 0.8;
+                      },
+                      onTapOutside: (e) {
+                        controller.passwordFocus.unfocus();
+                        controller.heightScale.value = 0.6;
+                      },
+                      onFieldSubmitted: (str) {
+                        controller.heightScale.value = 0.6;
+                        controller.onLogin();
+                      },
+                    ),
+                    SizedBox(
+                      height: height! * 0.6 * 0.1,
+                    ),
+                    Obx(() => (controller.logging.value)
+                        ? BasicButton(
+                            onPress: controller.onLogin,
+                            text: 'logging'.tr,
+                            icon: SizedBox(
+                              height: 25,
+                              width: 25,
+                              child: CircularProgressIndicator(
+                                color: AppColors.backColor,
                               ),
-                              size: Size(width! * 0.8, 40),
-                            )
-                          : BasicButton(
-                              onPress: controller.onLogin,
-                              text: 'login'.tr,
-                              icon: (Get.locale.toString() == "en_US")
-                                  ? Icon(
-                                      Icons.login,
-                                      color: AppColors.mainTextColor,
-                                    )
-                                  : RotatedBox(
-                                      quarterTurns: 2,
-                                      child: Icon(Icons.login,
-                                          color: AppColors.mainTextColor)),
-                              size: Size(width! * 0.8, 40),
-                            ))
-                    ],
-                  ),
-                ))
-          ],
+                            ),
+                            size: Size(width! * 0.8, 40),
+                          )
+                        : BasicButton(
+                            onPress: controller.onLogin,
+                            text: 'login'.tr,
+                            icon: (Get.locale.toString() == "en_US")
+                                ? Icon(
+                                    Icons.login,
+                                    color: AppColors.mainTextColor,
+                                  )
+                                : RotatedBox(
+                                    quarterTurns: 2,
+                                    child: Icon(Icons.login,
+                                        color: AppColors.mainTextColor)),
+                            size: Size(width! * 0.8, 40),
+                          ))
+                  ],
+                ),
+              )),
         )
       ],
     );
   }
-
-
 }
--- ibb_university_students_services/lib/app/views/login_view/web_login_view.dart
+++ ibb_university_students_services/lib/app/views/login_view/web_login_view.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
 import '../../components/buttons.dart';
 import '../../components/custom_text.dart';
 import '../../components/text_field.dart';
@@ -20,110 +21,158 @@ class WebLoginView extends StatelessWidget {
 
   LoginController controller = Get.find();
 
+  List<PopupMenuItem<String>> menuItems = [
+    PopupMenuItem<String>(value: "en", child: SecText("En")),
+    PopupMenuItem<String>(value: "ar", child: SecText("Ar")),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    double containerScale = 0.50;
     return Stack(
       fit: StackFit.expand,
       alignment: Alignment.bottomCenter,
       children: [
         SizedBox(
           child:
-              Image.asset("assets/images/loginbackimage.jpg", fit: BoxFit.fill),
+              Image.asset("assets/images/login_background_1.jpeg", fit: BoxFit.fill),
         ),
         Container(
           color: const Color.fromRGBO(0, 191, 255, 0.25),
         ),
         Column(
           children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                  height: height! * 0.05,
-                ),
-                Image.asset("assets/images/ibb_university_logo.png",
-                    fit: BoxFit.fitHeight),
-                MainText("IBB", fontWeight: FontWeight.bold, fontSize: 28),
-                MainText("UNIVERSITY",
-                    fontWeight: FontWeight.bold, fontSize: 28)
-              ],
+            SizedBox(
+              height: height! * 0.05,
             ),
+            Image.asset("assets/images/ibb_university_logo.png",
+                fit: BoxFit.fitHeight),
+            //container hold universityName
             Container(
-              height: (height! * containerScale),
-              width: width! * 0.35,
-              padding: const EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                color: AppColors.backColor,
-                borderRadius: const BorderRadius.all(Radius.circular(20)),
-              ),
-              child: Form(
-                key: controller.formKey,
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                child: Column(
-                  children: [
-                    MainText('login'.tr,
-                        fontWeight: FontWeight.bold,
-                        fontSize: height! * 0.15 / 4,
-                        textColor: Colors.black),
-                    SizedBox(
-                      height: height! * 0.6 * 0.1,
-                    ),
-                    CustomTextFormField(
-                      controller: controller.ID,
-                      validator: (id)=>controller.validateID(id),
-                      labelText: 'studentid'.tr,
-                      icon: Icons.account_circle_outlined,
-                    ),
-                    SizedBox(
-                      height: height! * 0.6 * 0.05,
-                    ),
-                    CustomTextFormField(
-                      controller: controller.password,
-                      validator: (pwd)=>controller.validatePassword(pwd),
-                      labelText: 'password'.tr,
-                      icon: Icons.key_sharp,
-                      isPassword: true,
-                    ),
-                    SizedBox(
-                      height: height! * 0.6 * 0.1,
-                    ),
-                    Obx(() => (controller.logging.value)
-                        ? BasicButton(
-                      onPress: (){},
-                      text: 'logging',
-                      icon: const CircularProgressIndicator(),
-                      size: Size(width! * 0.8, 40),
-                    )
-                        : BasicButton(
-                      onPress: controller.onLogin,
-                      text: 'login'.tr,
-                      icon: (Get.locale.toString() == "en_US")
-                          ? Icon(
-                        Icons.login,
-                        color: AppColors.mainTextColor,
-                      )
-                          : RotatedBox(
-                          quarterTurns: 2,
-                          child: Icon(Icons.login,
-                              color: AppColors.mainTextColor)),
-                      size: Size(width! * 0.8, 40),
-                    ))
+              decoration: const BoxDecoration(
+                  gradient: RadialGradient(
+                      colors: [
+                    Colors.transparent,
+                    Colors.black87,
+                    Colors.black54
                   ],
-                ),
-              )
+                      center: Alignment.bottomCenter,
+                      focal: Alignment.bottomCenter,
+                      radius: 0.1,
+                      focalRadius: 1)),
+              child: Column(
+                children: [
+                  MainText(
+                    "IBB",
+                    fontWeight: FontWeight.bold,
+                    fontSize: height! * 0.15 / 4,
+                  ),
+                  MainText(
+                    "UNIVERSITY",
+                    fontWeight: FontWeight.bold,
+                    fontSize: height! * 0.15 / 4,
+                  ),
+                ],
+              ),
             ),
+
+            Container(
+                width: width! * 0.35,
+                padding: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: AppColors.backColor,
+                  borderRadius: const BorderRadius.all(Radius.circular(20)),
+                ),
+                child: Form(
+                  key: controller.formKey,
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          SizedBox(
+                            width: width! * 0.35 * 0.2,
+                          ),
+                          MainText('login'.tr,
+                              fontWeight: FontWeight.bold,
+                              fontSize: height! * 0.15 / 4,
+                              textColor: Colors.black),
+
+                          PopupMenuButton<String>(
+                            initialValue: Get.locale?.languageCode.toString(),
+                            itemBuilder: (BuildContext context) => menuItems,
+                            onSelected: (lang) {
+                              controller.changeLang(lang);
+                            },
+                            child: Row(
+                              children:
+                              (Get.locale?.languageCode.toString() == "en")
+                                  ? [
+                                const Icon(Icons.language),
+                                SecText("En"),
+                              ]
+                                  : [
+                                SecText("Ar"),
+                                const Icon(Icons.language),
+                              ],
+                            ),
+                          ),
+
+                        ],
+                      ),
+                      SizedBox(
+                        height: height! * 0.6 * 0.1,
+                      ),
+                      CustomTextFormField(
+                          controller: controller.id,
+                          validator: (id) => controller.validateID(id),
+                          labelText: 'studentid'.tr,
+                          icon: Icons.account_circle_outlined,
+                          ),
+                      SizedBox(
+                        height: height! * 0.6 * 0.05,
+                      ),
+                      CustomTextFormField(
+                          controller: controller.password,
+                          validator: (pwd) => controller.validatePassword(pwd),
+                          labelText: 'password'.tr,
+                          icon: Icons.key_sharp,
+                          isPassword: true,
+                          onFieldSubmitted: (e) {
+                            controller.onLogin();
+                          }),
+                      SizedBox(
+                        height: height! * 0.6 * 0.1,
+                      ),
+                      Obx(() => (controller.logging.value)
+                          ? BasicButton(
+                              onPress: controller.onLogin,
+                              text: 'logging',
+                              icon: CircularProgressIndicator(
+                                color: AppColors.backColor,
+                              ),
+                              size: Size(width! * 0.8, 40),
+                            )
+                          : BasicButton(
+                              onPress: controller.onLogin,
+                              text: 'login'.tr,
+                              icon: (Get.locale.toString() == "en_US")
+                                  ? Icon(
+                                      Icons.login,
+                                      color: AppColors.mainTextColor,
+                                    )
+                                  : RotatedBox(
+                                      quarterTurns: 2,
+                                      child: Icon(Icons.login,
+                                          color: AppColors.mainTextColor)),
+                              size: Size(width! * 0.8, 40),
+                            ))
+                    ],
+                  ),
+                )),
           ],
         )
       ],
     );
-  }
-
-  void loginButton() {
-    if (Get.locale.toString() == "en_US") {
-      Get.updateLocale(const Locale('ar'));
-    } else {
-      Get.updateLocale(const Locale('en_US'));
-    }
   }
 }
