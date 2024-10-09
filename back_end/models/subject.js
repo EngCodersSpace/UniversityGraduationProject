d4693e44b02858cf1246a788ad79d1fe101aed01
Hameed-Najeeb <hameednjeeb1@gmail.com>
2024-10-08 19:35:50 -0700

    add exam table and its  relationship with (subject  table) and add Constraint and add the database scheme (4)

--- back_end/models/subject.js
+++ back_end/models/subject.js
@@ -22,9 +22,14 @@ module.exports = (sequelize, DataTypes) => {
         foreignKey: 'subject_id', //the foreign Key in the grade table refers to subject table
       });
 
-       //(3)Relationship One-to-Many between "subject table" and  "prerequisite table"
-       subject.hasMany(models.prerequisite, {
+      //(3)Relationship One-to-Many between "subject table" and  "prerequisite table"
+      subject.hasMany(models.prerequisite, {
         foreignKey: 'subject_id', //the foreign Key in the prerequisite table refers to subject table
+      });
+
+      //(4)Relationship One-to-Many between "subject table" and  "exam table"
+      subject.hasMany(models.exam, {
+        foreignKey: 'subject_id', //the foreign Key in the exam table refers to subject table
       });
 
     }
@@ -39,9 +44,9 @@ module.exports = (sequelize, DataTypes) => {
     subject_name: {
       type: DataTypes.STRING
     },
-    number_of_units:{
-      type:DataTypes.TINYINT,
-      allowNull:false,
+    number_of_units: {
+      type: DataTypes.TINYINT,
+      allowNull: false,
     },
     subject_description: {
       type: DataTypes.STRING,
