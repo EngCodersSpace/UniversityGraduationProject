334387cf90c24b32f07d1b5dcf75d70d686937aa
Ahmedalgbri12 <151460573+Ahmedalgbri12@users.noreply.github.com>
2024-10-12 18:04:32 +0300

    after update migration

--- back_end/models/grade.js
+++ back_end/models/grade.js
@@ -105,3 +105,7 @@ module.exports = (sequelize, DataTypes) => {
   });
   return grade;
 };
+
+
+
+
--- back_end/server.js
+++ back_end/server.js
@@ -1,21 +1,74 @@
 //  server.js
 const express = require('express');
 const dotenv = require("dotenv");
-// const { user } = require('./models'); 
-// const router = express.Router();
+const { user } = require('./models'); 
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const SECRET_KEY = process.env.SECRET_KEY ;
 
 dotenv.config({ path: './.env' });
 const app = express();
 app.use(express.json());
 
-// const easyRoute = require('./routes/esayRoute')
-// app.use('/api', easyRoute);
-// const userRoutes = require('./routes/userRoute');
+function generateToken(params = {}) {
+    return jwt.sign(params,SECRET_KEY , {
+        expiresIn: 78300, // مدة انتهاء الصلاحية (يمكن تعديلها حسب الحاجة)
+    });
+}
 
-const authRoutes = require('./routes/authRoute')
+async function login(req, res) {
+        const { id, password, islogged } = req.body; 
 
-app.use(authRoutes);
-// app.use('/api', userRoutes);
+        try {
+            const userRecord = await user.findOne({ where: { user_id: id } });
+
+            if (!userRecord) {
+                return res.status(400).send({
+                    status: 0,
+                    message: 'User ID or password is incorrect!',
+                    user: {}
+                });
+            }
+
+            if (!bcrypt.compareSync(password, userRecord.password)) {
+                return res.status(400).send({
+                    status: 0,
+                    message: 'User ID or password is incorrect!',
+                    user: {}
+                });
+            }
+
+            const user_id = userRecord.user_id; 
+
+            await user.update({
+                islogged
+            }, {
+                where: {
+                    id: user_id
+                }
+            });
+
+            userRecord.password = undefined; 
+
+            const token = generateToken({ user_id: userRecord.user_id });
+
+            return res.status(200).send({
+                status: 1,
+                message: "User logged in successfully!",
+                user: userRecord,
+                token
+            });
+
+        } catch (error) {
+            return res.status(500).send({
+                status: 0,
+                message: 'Server error',
+                error: error.message
+            });
+        }
+}
+
+app.post('/login', login);
 
 const PORT = process.env.PORT ;
 // Start server
@@ -32,5 +85,19 @@ app.listen(PORT, () => {
 
 
 
+
+
+
+
+
+
+
+
+// const authRoutes = require('./routes/authRoute')
+// app.use(authRoutes);
+
+// const easyRoute = require('./routes/esayRoute')
+// app.use('/api', easyRoute);
+// const userRoutes = require('./routes/userRoute');
 
 
