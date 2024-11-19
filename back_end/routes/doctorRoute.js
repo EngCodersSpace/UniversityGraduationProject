b778dccc02c0ecf87989b78b6aef273e65c3dcf2
Ahmedalgbri12 <151460573+Ahmedalgbri12@users.noreply.github.com>
2024-11-20 01:39:08 +0300

    test 2 models successfuly doctor and student with CRUD WITHIN (authin & authoriz) ,checkRole

--- back_end/routes/doctorRoute.js
+++ back_end/routes/doctorRoute.js
@@ -1,21 +1,21 @@
 // doctorRoute.js
-const express = require('express');
-const router = express.Router();
-const doctorController = require('../controllers/doctorController');
-const authToken = require('../middleware/authMiddleware');
-const checkRole = require('../middleware/roleMiddleware');
+// const express = require('express');
+// const router = express.Router();
+// const doctorController = require('../controllers/doctorController');
+// const authToken = require('../middleware/authMiddleware');
+// const checkRole = require('../middleware/roleMiddleware');
 
-// Apply authentication middleware for all routes
-router.use(authToken.verifyToken);
+// // Apply authentication middleware for all routes
+// router.use(authToken.verifyToken);
 
-// CRUD routes
-router.get('/doctors',checkRole('teacher'), doctorController.getAllDoctors); 
-router.get('/doctors/:id', checkRole('admin', 'teacher'), doctorController.getDoctorById); 
-router.post('/doctors', checkRole('admin'), doctorController.createDoctor); 
-router.put('/doctors/:id', checkRole('admin'), doctorController.updateDoctor); 
-router.delete('/doctors/:id', checkRole('admin'), doctorController.deleteDoctor); 
+// // CRUD routes
+// router.get('/doctors',checkRole('teacher'), doctorController.getAllDoctors); 
+// router.get('/doctors/:id', checkRole('admin', 'teacher'), doctorController.getDoctorById); 
+// router.post('/doctors', checkRole('admin'), doctorController.createDoctor); 
+// router.put('/doctors/:id', checkRole('admin'), doctorController.updateDoctor); 
+// router.delete('/doctors/:id', checkRole('admin'), doctorController.deleteDoctor); 
 
-module.exports = router;
+// module.exports = router;
 
 
 
