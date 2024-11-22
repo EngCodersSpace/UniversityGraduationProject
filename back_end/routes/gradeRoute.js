19548f2a426798a8fdaf8014543dceec4285bbfc
Ahmedalgbri12 <151460573+Ahmedalgbri12@users.noreply.github.com>
2024-11-22 21:13:39 +0300

    Do CRUD for Lecture model and route and validation

--- back_end/routes/gradeRoute.js
+++ back_end/routes/gradeRoute.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const gC = require('../controllers/gradeController');
 const vali = require('../validations/gradevalidation');
 
-router.post('/creat-grade', vali.createGrade,   gC.createGrade);
+router.post('/create-grade', vali.createGrade,   gC.createGrade);
 router.get('/get-all-grades',   gC.getAllGrades);
 router.get('/get-grade/:id',    gC.getGradeById);
 router.put('/update-grade/:id',vali.updateGrade, gC.updateGrade);
