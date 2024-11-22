76b82d05eed5e4d2580fc77eb7228e2fc274cfcf
Hameed-Najeeb <hameednjeeb1@gmail.com>
2024-11-21 17:50:09 -0800

    add level table

---
+++ back_end/models/level.js
@@ -0,0 +1,35 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class level extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  level.init({
+
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    level_name: {
+      type:DataTypes.STRING(50),
+      allowNull:false,
+    },
+
+
+  }, {
+    sequelize,
+    modelName: 'level',
+  });
+  return level;
+};
