de2456b4a801c071b1c8ffa58cc94519d132265e
SHEHAB <shshehab02@gmail.com>
2024-11-24 04:39:29 +0300

    merage FrontEnt at 12120f13684d4ea3ba39b30639deb24ba2d7882a to phoneScreens

---
+++ ibb_university_students_services/lib/app/views/profile_tab_view/web_profile_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:ibb_university_students_services/app/controllers/tabs_controller/profile_tab_controller.dart';
+
+class WebProfileView extends GetView<ProfileController> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ ibb_university_students_services/lib/app/views/table_tab_view/web_table_tab_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:ibb_university_students_services/app/controllers/tabs_controller/table_tab_view_controller.dart';
+
+class WebTableTabView extends GetView<TableTabController> {
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+}
