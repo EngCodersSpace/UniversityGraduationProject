1d32ee6b203c223d64fd41035d630add9c522b8a
SHEHAB <shshehab02@gmail.com>
2024-11-25 00:19:22 +0300

    merge Ahmed at to 070ceef54e35fd84d56bc6103b1036422ac45953 to phoneScreens

---
+++ back_end/middleware/authMiddleware.js
@@ -0,0 +1,44 @@
+
+// authMiddleware.js    to verify the JWT token
+const jwt = require('jsonwebtoken');
+const dotenv = require("dotenv");
+dotenv.config({ path: './.env' });
+
+exports.verifyToken = (req, res, next) => {
+    const bearerHeader = req.headers.authorization;
+    if (!bearerHeader) {
+        return res.status(401).json({ message: "Authorization header is missing" });
+    }
+
+    const token = bearerHeader.split(' ')[1];
+    if (!token) {
+        return res.status(401).json({ message: "Token is missing" });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.SECRET_KEY); 
+        req.user = decoded;
+        next();
+    } catch (err) {
+        return res.status(403).json({ message: "Invalid or expired token" });
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
