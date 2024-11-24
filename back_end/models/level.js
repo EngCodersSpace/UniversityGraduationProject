dcbcdd3192b7d1536979b343ad8b402bdd9677a2
Hameed-Najeeb <hameednjeeb1@gmail.com>
2024-11-23 19:19:55 -0800

    add Relationship One-to-Many between lecture table and  level table

--- back_end/models/level.js
+++ back_end/models/level.js
@@ -13,19 +13,24 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
 
       //(1) Relationship One-to-Many between "student table" and  "level table"
-      level.hasMany(models.student,{
-        foreignKey:'student_level_id',
+      level.hasMany(models.student, {
+        foreignKey: 'student_level_id',
       });
 
       //(2) Relationship One-to-Many between "study_plan_elment table" and  "level table"
-      level.hasMany(models.study_plan_elment,{
-        foreignKey:'level_id',
-      });      
+      level.hasMany(models.study_plan_elment, {
+        foreignKey: 'level_id',
+      });
 
       //(3) Relationship One-to-Many between "student_fee table" and  "level table"
-      level.hasMany(models.student_fee,{
-        foreignKey:'level_fees_id',
-      });            
+      level.hasMany(models.student_fee, {
+        foreignKey: 'level_fees_id',
+      });
+
+      //(4) Relationship One-to-Many between "lecture table" and  "level table"
+      level.hasMany(models.lecture, {
+        foreignKey: 'lecture_level_id',
+      });
 
 
     }
@@ -39,8 +44,8 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.INTEGER
     },
     level_name: {
-      type:DataTypes.STRING(50),
-      allowNull:false,
+      type: DataTypes.STRING(50),
+      allowNull: false,
     },
 
 
