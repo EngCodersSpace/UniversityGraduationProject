5adb45f9aff712eb570103cd59b1b95917847187
Ahmedalgbri12 <151460573+Ahmedalgbri12@users.noreply.github.com>
2024-11-28 22:52:05 +0300

    Do CRUD lecture controll

--- back_end/routes/lectureRoute.js
+++ back_end/routes/lectureRoute.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const { createLecture, getLectures, getLectureById, updateLecture, deleteLecture } = require('../controllers/lectureController');
+const { 
+    createLecture,
+    getLectures,
+    getLectureById, 
+    updateLecture, 
+    deleteLecture,
+    getLecturesBySection,
+    getLecturesByLevel,
+    getLecturesByDoctor,
+    getLectureBySubject
+ } = require('../controllers/lectureController');
 const vali=require('../validations/lecturevalidation')
 
 router.post('/create-lecture', vali.createLectureValidator ,createLecture);
@@ -12,5 +22,11 @@ router.get('/get-lecture/:id', getLectureById);
 router.put('/update-lecture/:id',vali.updateLectureValidator, updateLecture);
 
 router.delete('/delete-lecture/:id', deleteLecture);
+
+router.get('/lectures/section/:sectionId', getLecturesBySection);
+router.get('/lectures/level/:levelId', getLecturesByLevel);
+router.get('/lectures/doctor/:doctorId', getLecturesByDoctor);
+router.get('/lectures/subject/:subjectId', getLectureBySubject);
+
 
 module.exports = router;
