0437cb06a787fbeeb728bbebde62c3847215c733
Mohammed Bazrah <amoh05313@gmail.com>
2024-11-29 00:15:20 +0300

    modify to first work of cards of web home tab

--- ibb_university_students_services/lib/app/views/home_tab_view/web_home_tab.dart
+++ ibb_university_students_services/lib/app/views/home_tab_view/web_home_tab.dart
@@ -11,97 +11,114 @@ class WebHomeTab extends GetView<HomeTabController> {
   });
   double hight = Get.height;
   double width = Get.width;
-  late double cardsize = (width - width * 0.35);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-      child: SingleChildScrollView(
-          padding: EdgeInsets.symmetric(vertical: 30, horizontal: 20),
-          child: Container(
-            height: hight - 600,
-            width: width - 400,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Obx(
-                  () => (controller.initState.value)
-                      ? InkWell(
-                          onTap: () {},
-                          child: Card(
-                            color: Color.fromARGB(211, 105, 187, 243),
-                            //surfaceTintColor: Color.fromARGB(214, 9, 98, 157),
-                            elevation: 10.0,
-                            child: Row(
-                              mainAxisSize: MainAxisSize.max,
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: [
-                                Image.asset(
-                                  "assets/image/bookshelf_4797659.png",
-                                  width: width * 1 / 12,
-                                  height: hight * 1 / 12,
-                                ),
-                                Column(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  children: [
-                                    MainText(
-                                      "Library",
-                                      textColor: AppColors.tabBackColor,
-                                    ),
-                                    SecText(
-                                      "this library gives you what you need during your educational career",
-                                      fontSize: 12,
-                                      textColor: AppColors.tabBackColor,
-                                    )
-                                  ],
-                                ),
-                              ],
+        body: Container(
+      color: AppColors.tabBackColor,
+      height: hight,
+      width: width * 0.8,
+      child: SafeArea(
+        child: SingleChildScrollView(
+            padding: EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+            child: Container(
+              height: hight - 600,
+              width: width - 400,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Obx(
+                    () => (controller.initState.value)
+                        ? InkWell(
+                            onTap: () {},
+                            child: Container(
+                              decoration: BoxDecoration(
+                                  gradient: LinearGradient(
+                                    colors: [
+                                      AppColors.inverseCardColor,
+                                      AppColors.inverseCardColor
+                                          .withOpacity(0.8),
+                                      AppColors.inverseCardColor
+                                          .withOpacity(0.6),
+                                      AppColors.inverseCardColor
+                                          .withOpacity(0.8),
+                                      AppColors.inverseCardColor
+                                    ],
+                                    begin: Alignment.center,
+                                    end: Alignment.centerLeft,
+                                  ),
+                                  borderRadius: BorderRadius.circular(24)),
+                              child: Row(
+                                mainAxisSize: MainAxisSize.max,
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  Image.asset(
+                                    "assets/images/bookshelf_4797659.png",
+                                    width: width * 1 / 12,
+                                    height: hight * 1 / 12,
+                                  ),
+                                  Column(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: [
+                                      MainText(
+                                        "Library",
+                                        textColor: AppColors.tabBackColor,
+                                      ),
+                                      // SecText(
+                                      //   "this library gives you what you need during your educational career",
+                                      //   fontSize: 12,
+                                      //   textColor: AppColors.tabBackColor,
+                                      // ),
+                                    ],
+                                  ),
+                                ],
+                              ),
                             ),
-                          ),
-                        )
-                      : const Placeholder(),
-                ),
-                Obx(
-                  () => (controller.initState.value)
-                      ? InkWell(
-                          onTap: () {},
-                          child: Card(
-                            color: Color.fromARGB(211, 105, 187, 243),
-                            //surfaceTintColor: Color.fromARGB(214, 9, 98, 157),
-                            elevation: 10.0,
-                            child: Row(
-                              mainAxisSize: MainAxisSize.max,
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: [
-                                Image.asset(
-                                  "assets/image/bookshelf_4797659.png",
-                                  width: width * 1 / 12,
-                                  height: hight * 1 / 12,
-                                ),
-                                Column(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  children: [
-                                    MainText(
-                                      "Library",
-                                      textColor: AppColors.tabBackColor,
-                                    ),
-                                    SecText(
-                                      "this library gives you what you need during your educational career",
-                                      fontSize: 12,
-                                      textColor: AppColors.tabBackColor,
-                                    )
-                                  ],
-                                ),
-                              ],
+                          )
+                        : const Placeholder(),
+                  ),
+                  Obx(
+                    () => (controller.initState.value)
+                        ? InkWell(
+                            onTap: () {},
+                            child: Card(
+                              color: Color.fromARGB(211, 105, 187, 243),
+                              //surfaceTintColor: Color.fromARGB(214, 9, 98, 157),
+                              elevation: 10.0,
+                              child: Row(
+                                mainAxisSize: MainAxisSize.max,
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  Image.asset(
+                                    "assets/images/book.png",
+                                    width: width * 1 / 7,
+                                    height: hight * 1 / 7,
+                                  ),
+                                  Column(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: [
+                                      MainText(
+                                        "Library",
+                                        textColor: AppColors.tabBackColor,
+                                      ),
+                                      // SecText(
+                                      //   "this library gives you what you need during your educational career",
+                                      //   fontSize: 12,
+                                      //   textColor: AppColors.tabBackColor,
+                                      // ),
+                                    ],
+                                  ),
+                                ],
+                              ),
                             ),
-                          ),
-                        )
-                      : const Placeholder(),
-                ),
-              ],
-            ),
-          )),
+                          )
+                        : const Placeholder(),
+                  ),
+                ],
+              ),
+            )),
+      ),
     ));
   }
 }
