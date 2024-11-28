84b4c3048fc8ab99f5c10c62cb7ab5055def960e
Ahmedalgbri12 <151460573+Ahmedalgbri12@users.noreply.github.com>
2024-11-28 22:52:05 +0300

    Do CRUD lecture controll

--- back_end/controllers/lectureController.js
+++ back_end/controllers/lectureController.js
@@ -1,5 +1,6 @@
 const { validationResult } = require('express-validator');
-const { lecture, subject, doctor } = require('../models'); 
+const { lecture, subject, doctor ,section , level } = require('../models'); 
+const { Sequelize} = require('sequelize');
 
 const createLecture = async (req, res) => {
   try {
@@ -18,72 +19,6 @@ const createLecture = async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Error creating lecture', error: error.message });
-  }
-};
-
-const getLectures = async (req, res) => {
-  try {
-    const lectures = await lecture.findAll({
-      include: [
-        {
-          model: subject,
-          as: 'subject'
-        },
-        {
-          model: doctor,
-          as: 'doctor'
-        },
-        {
-          model:section,
-          as:'section'
-        },
-        {
-          model:level,
-          as:'level'
-        },
-      ],
-    });
-    res.status(200).json({message:'getAllLectures', data: lectures });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Error retrieving lectures', error: error.message });
-  }
-};
-
-const getLectureById = async (req, res) => {
-  try {
-    const { id } = req.params;
-
-    const lectureDetails = await lecture.findOne({
-      where: { id },
-      include: [
-        {
-          model: subject,
-          as: 'subject'
-        },
-        {
-          model: doctor,
-          as: 'doctor'
-        },
-        // {
-        //   model: section,
-        //   as: 'section'
-        // },
-        {
-          model: level,
-          as: 'level'
-        },
-      ],
-    });
-
-    if (!lectureDetails) {
-      return res.status(404).json({ message: 'Lecture not found' });
-    }
-
-    res.status(200).json({ data: lectureDetails });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Error retrieving lecture', error: error.message });
   }
 };
 
@@ -147,10 +82,176 @@ const deleteLecture = async (req, res) => {
   }
 };
 
+
+
+
+const getLectures = async (req, res) => {
+  try {
+    const lectures = await lecture.findAll();
+    res.status(200).json({message:'getAllLectures', data: lectures });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error retrieving lectures', error: error.message });
+  }
+};
+
+const getLectureById = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const lectureDetails = await lecture.findOne({
+      where: { id },
+      include: [
+        {
+          model: subject,
+          as: 'subject'
+        },
+        {
+          model: doctor,
+          as: 'doctor'
+        },
+        {
+          model:section,
+          as:'section'
+        },
+        {
+          model:level,
+          as:'level'
+        },
+      ],
+    });
+
+    if (!lectureDetails) {
+      return res.status(404).json({ message: 'Lecture not found' });
+    }
+
+    res.status(200).json({message: `get lecture by id : ${id} `, data: lectureDetails });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error retrieving lecture', error: error.message });
+  }
+};
+
+const getLecturesBySection = async (req, res) => {
+  try {
+    const { sectionId } = req.params; 
+    const lectures = await lecture.findAll({
+      where: {
+        lecture_section_id: sectionId,
+      },
+      include: [
+        { model: subject, as: 'subject' }, 
+        { model: doctor, as:'doctor' },  
+      ],
+    });
+
+    if (!lectures.length) {
+      return res.status(404).json({ message: 'No lectures found for this section' });
+    }
+
+    res.status(200).json({ message: `Lectures for section id: ${sectionId}`, data: lectures });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error retrieving lectures by section', error: error.message });
+  }
+};
+
+const getLecturesByLevel = async (req, res) => {
+  try {
+    const { levelId } = req.params; 
+
+    const lectures = await lecture.findAll({
+      where: {
+        lecture_level_id: levelId,
+      },
+      include: [
+        { model: subject, as: 'subject' },
+        { model: doctor, as:'doctor' },
+      ],
+    });
+
+    if (!lectures.length) {
+      return res.status(404).json({ message: 'No lectures found for this level' });
+    }
+
+    res.status(200).json({ message: `Lectures for level id: ${levelId}`, data: lectures });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error retrieving lectures by level', error: error.message });
+  }
+};
+
+const getLecturesByDoctor = async (req, res) => {
+  try {
+    const { doctorId } = req.params;      
+    const lectures = await lecture.findAll({
+      where: {
+        doctor_id: doctorId,
+      },
+      include: [
+        { model: subject, as: 'subject' },
+        { model: section, as: 'section' },
+        { model: level,   as: 'level' },
+      ],
+      attributes: {
+          include: [
+              [Sequelize.fn('COUNT', Sequelize.col('lecture.id')), 'lectureCount'] 
+          ]
+      },
+      group: ['lecture.id'],
+    });
+
+    if (!lectures.length) {
+      return res.status(404).json({ message: 'No lectures found for this doctor' });
+    }
+
+    res.status(200).json({ message: `Lectures for doctor id: ${doctorId}`, data: lectures });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error retrieving lectures by doctor', error: error.message });
+  }
+};
+
+const getLectureBySubject = async (req, res) =>{
+  try {
+    const { subjectId } = req.params;
+    const lectures = await lecture.findAll({
+      where:{
+        subject_id : subjectId,
+      },
+      include:[
+        {model: doctor,  as: 'doctor' },
+        {model: level,   as: 'level'  },
+        {model: section, as: 'section'},
+      ],
+    });
+
+    if (!lectures.length) {
+      return res.status(404).json({ message: 'No lectures found for this subject' });
+    }
+    res.status(200).json({ message: `Lectures for subject ${subjectId}`, data: lectures });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error retrieving lectures by doctor', error: error.message });
+  }
+};
+
+
+
+
+
+
+
+
+
 module.exports = {
   createLecture,
   getLectures,
   getLectureById,
   updateLecture,
   deleteLecture,
+  getLecturesBySection,
+  getLecturesByLevel,
+  getLecturesByDoctor,
+  getLectureBySubject
 };
