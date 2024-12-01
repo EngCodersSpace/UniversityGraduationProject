0ba723793d182af560fb62665fef0442715b3040
Ahmedalgbri12 <151460573+Ahmedalgbri12@users.noreply.github.com>
2024-12-01 23:20:13 +0300

    Do cors(corsOptions) on getdata to test

--- back_end/routes/dataRoute.js
+++ back_end/routes/dataRoute.js
@@ -2,7 +2,8 @@
 const express = require('express');
 const { getAllData } = require('../controllers/dataControll');
 const router = express.Router();
+const cors = require('cors');
 
-router.get('/all-data', getAllData); 
+router.get('/all-data', cors(corsOptions), getAllData); 
 
 module.exports = router;
