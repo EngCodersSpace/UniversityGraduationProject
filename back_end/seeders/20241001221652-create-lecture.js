69ae7e14f7b36066ba1223558739c7dfa8c9da7d
SHEHAB <shshehab02@gmail.com>
2024-12-03 01:01:12 +0300

    merge ModelsAndMigrations at 89bdbd4c3bf4083da60efb978267aeca52b45779 to Amed

--- back_end/seeders/20241001221652-create-lecture.js
+++ back_end/seeders/20241001221652-create-lecture.js
@@ -5,22 +5,17 @@ const { lecture, subject, doctor, section, level } = require('../models');
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    
+
     const subjects = await subject.findAll();
     const doctors = await doctor.findAll();
     const sections = await section.findAll();
     const levels = await level.findAll();
 
     const lectures = [];
-    const daysOfWeek = ['Saturday', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday'];
-
-    for (let i = 0; i < Math.min(subjects.length, doctors.length, 6); i++) { // Create 6 lectures for each day of the week
-      const randomHour = faker.number.int({ min: 8, max: 17 });
-      const randomMinute = faker.number.int({ min: 0, max: 59 });
-      const lectureTime = `${randomHour.toString().padStart(2, '0')}:${randomMinute.toString().padStart(2, '0')}`;
-
-      // Generate a random hall from Hall 1 to Hall 100
-      const lecture_room = `Hall ${faker.number.int({ min: 1, max: 100 })}`;
+    for (let i = 0; i < Math.min(subjects.length, doctors.length, 50); i++) { 
+      const randomHour = faker.number.int({ min: 8, max: 17 }); 
+      const randomMinute = faker.number.int({ min: 0, max: 59 }); 
+      const lectureTime = `${randomHour.toString().padStart(2, '0')}:${randomMinute.toString().padStart(2, '0')}`; 
 
       lectures.push({
         subject_id: subjects[i % subjects.length].subject_id,
@@ -28,21 +23,22 @@ module.exports = {
         lecture_section_id: sections[i % sections.length].id,
         lecture_level_id: levels[i % levels.length].id,
         term: faker.helpers.arrayElement(['Term 1', 'Term 2']),
-        year: new Date().getFullYear(), // Set the year to the current year
-        lecture_time: lectureTime,
-        lecture_duration: faker.number.int({ min: 1, max: 400 }), // Adjust as needed for lecture duration
-        lecture_day: daysOfWeek[i], // Assign each lecture to a different day
-        lecture_room: lecture_room, // Set the lecture room to a random hall
+        year: faker.date.past().getFullYear(), 
+        lecture_time: lectureTime, 
+        lecture_duration: `${faker.number.int({ min: 1, max: 3 })} hours`, 
+        lecture_day: faker.helpers.arrayElement(['Saturday', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday']),
+        lecture_room: faker.string.alphanumeric(5), 
         createdAt: new Date(),
         updatedAt: new Date(),
       });
     }
 
-    // Insert the lecture records
+    
     await lecture.bulkCreate(lectures);
   },
 
   down: async (queryInterface, Sequelize) => {
+    
     await queryInterface.bulkDelete('lectures', null, {});
   }
 };
