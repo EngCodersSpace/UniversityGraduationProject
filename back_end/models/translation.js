f312be1598d827de2a12aec32aa25fa82169dc97
Hameed-Najeeb <hameednjeeb1@gmail.com>
2024-12-02 17:09:53 -0800

    add translation table

---
+++ back_end/models/translation.js
@@ -0,0 +1,48 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class translation extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  translation.init({
+         id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      tableName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      recordId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      field: {
+        type: DataTypes.STRING,
+        allowNull:false,
+      },
+      language:{
+        type:DataTypes.INTEGER,
+        allowNull:false,
+      },
+      value:{
+        type:DataTypes.STRING,
+        allowNull:false,
+      },
+  }, {
+    sequelize,
+    modelName: 'translation',
+  });
+  return translation;
+};
