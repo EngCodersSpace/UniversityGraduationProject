44fd904e1839168aa0cc919f695a6e5c0609b2bc
Mohammed Bazrah <amoh05313@gmail.com>
2024-12-16 22:51:06 +0300

    no thing

--- ibb_university_students_services/lib/app/views/table_tab_view/web_table_tab_view.dart
+++ ibb_university_students_services/lib/app/views/table_tab_view/web_table_tab_view.dart
@@ -21,8 +21,10 @@ class WebTableTabView extends GetView<TableTabController> {
         : Stack(
             children: [
               Container(
+                width: width,
+                height: height,
                 color: AppColors.tabBackColor,
-                padding: const EdgeInsets.all(20),
+                padding: const EdgeInsets.all(10),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
@@ -50,33 +52,33 @@ class WebTableTabView extends GetView<TableTabController> {
                             )),
                       ),
                     ),
-                    SizedBox(
-                      width: width * 0.1,
-                    ),
-                    SecText(
-                      "Level",
-                      fontWeight: FontWeight.bold,
-                    ),
-                    SizedBox(
-                      width: width * 0.02,
-                    ),
-                    Container(
-                      width: width * 0.2,
-                      decoration: BoxDecoration(
-                        color: AppColors.inverseIconColor,
-                        borderRadius: BorderRadius.circular(24),
-                      ),
-                      child: Center(
-                        child: Obx(() => DropdownButton(
-                              items: controller.levels,
-                              onChanged: controller.changeLevel,
-                              value: controller.selectedLevel.value,
-                              underline: const SizedBox(),
-                              iconEnabledColor: AppColors.mainCardColor,
-                              dropdownColor: AppColors.inverseCardColor,
-                            )),
-                      ),
-                    ),
+                    // SizedBox(
+                    //   width: width * 0.1,
+                    // ),
+                    // SecText(
+                    //   "Level",
+                    //   fontWeight: FontWeight.bold,
+                    // ),
+                    // SizedBox(
+                    //   width: width * 0.02,
+                    // ),
+                    // Container(
+                    //   width: width * 0.2,
+                    //   decoration: BoxDecoration(
+                    //     color: AppColors.inverseIconColor,
+                    //     borderRadius: BorderRadius.circular(24),
+                    //   ),
+                    //   child: Center(
+                    //     child: Obx(() => DropdownButton(
+                    //           items: controller.levels,
+                    //           onChanged: controller.changeLevel,
+                    //           value: controller.selectedLevel.value,
+                    //           underline: const SizedBox(),
+                    //           iconEnabledColor: AppColors.mainCardColor,
+                    //           dropdownColor: AppColors.inverseCardColor,
+                    //         )),
+                    //   ),
+                    // ),
                     SizedBox(
                       width: width * 0.1,
                     ),
@@ -136,7 +138,94 @@ class WebTableTabView extends GetView<TableTabController> {
                     ),
                   ],
                 ),
-              )
+              ),
+              SingleChildScrollView(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      width: width * 0.6,
+                      child: Table(
+                        border: TableBorder.all(
+                            width: 1,
+                            color: Colors.black,
+                            style: BorderStyle.solid),
+                        defaultVerticalAlignment:
+                            TableCellVerticalAlignment.middle,
+                        children: [
+                          TableRow(
+                              decoration: BoxDecoration(
+                                  color: AppColors.inverseCardColor
+                                      .withOpacity(0.2)),
+                              children: [
+                                TableCell(
+                                  verticalAlignment:
+                                      TableCellVerticalAlignment.middle,
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8),
+                                    child: SecText("Level 1"),
+                                  ),
+                                ),
+                                TableCell(
+                                  verticalAlignment:
+                                      TableCellVerticalAlignment.middle,
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8),
+                                    child: SecText("Level 2"),
+                                  ),
+                                ),
+                                TableCell(
+                                  verticalAlignment:
+                                      TableCellVerticalAlignment.middle,
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8),
+                                    child: SecText("Level 3"),
+                                  ),
+                                ),
+                                TableCell(
+                                  verticalAlignment:
+                                      TableCellVerticalAlignment.middle,
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8),
+                                    child: SecText("Level 4"),
+                                  ),
+                                ),
+                                TableCell(
+                                  verticalAlignment:
+                                      TableCellVerticalAlignment.middle,
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8),
+                                    child: SecText("Level 5"),
+                                  ),
+                                ),
+                              ]),
+                          TableRow(
+                              decoration: BoxDecoration(
+                                color: AppColors.tabBackColor,
+                              ),
+                              children: [
+                                TableCell(
+                                    child: Padding(
+                                  padding: const EdgeInsets.all(8),
+                                  child: SecText("Saterday"),
+                                )),
+                                TableCell(
+                                    child: Padding(
+                                  padding: const EdgeInsets.all(8),
+                                  child: SecText(
+                                      "${controller.selectedDay.value}"),
+                                ))
+                              ]),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      width: width * 0.2,
+                    ),
+                  ],
+                ),
+              ),
             ],
           ));
   }
