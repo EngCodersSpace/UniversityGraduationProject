5688ac69dbae0e24b850b0df374a9e2665f3f570
SHEHAB <shshehab02@gmail.com>
2024-12-18 02:11:15 +0300

    build payments view

---
+++ ibb_university_students_services/lib/app/views/payments_view/payments_view_components/payments_level_card.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:ibb_university_students_services/app/models/student_fee.dart';
+import 'package:ibb_university_students_services/app/models/student_model.dart';
+import 'package:ibb_university_students_services/app/styles/app_colors.dart';
+import '../../../components/custom_text_v2.dart';
+import '../../../styles/text_styles.dart';
+
+class PaymentLevelCard extends GetView {
+  String title;
+  List<StudentFee> payments;
+
+  PaymentLevelCard({required this.title, required this.payments, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ExpansionTile(
+      title: CustomText(
+        title,
+        style: AppTextStyles.mainStyle(textHeader: AppTextHeaders.h2),
+      ),
+      backgroundColor: AppColors.inverseCardColor,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)),
+      collapsedShape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)),
+      collapsedIconColor: AppColors.mainCardColor,
+      iconColor: AppColors.mainCardColor,
+      collapsedBackgroundColor: AppColors.inverseCardColor,
+      childrenPadding: EdgeInsets.all(8),
+      children: [
+        CustomText(
+          title,
+          style: AppTextStyles.mainStyle(textHeader: AppTextHeaders.h2),
+        ),
+        CustomText(
+          title,
+          style: AppTextStyles.mainStyle(textHeader: AppTextHeaders.h2),
+        ),
+        CustomText(
+          title,
+          style: AppTextStyles.mainStyle(textHeader: AppTextHeaders.h2),
+        ),
+      ],
+    );
+  }
+}
