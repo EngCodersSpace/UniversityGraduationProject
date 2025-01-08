84e0f815d6029da1004667fea4c64d5599d0273c
Ahmedalgbri12 <151460573+Ahmedalgbri12@users.noreply.github.com>
2025-01-08 23:29:42 +0300

    edit book model ang his router

--- back_end/routes/bookRoute.js
+++ back_end/routes/bookRoute.js
@@ -2,12 +2,15 @@
 
 const express = require('express');
 const router = express.Router();
-const bookController = require('../controllers/bookController');
+const CRUD = require('../controllers/bookController');
 const { verifyToken } = require('../middleware/authMiddleware');
 
 router.use(verifyToken);
 
-router.post('/upload',verifyToken,  bookController.uploadFile);
-router.get('/download', bookController.downloadFile);
+router.post('/upload',verifyToken,  CRUD.uploadFile);
+router.get('/download', CRUD.downloadFile);
+router.get('/get-all-books', CRUD.getBooksByCategory);
+router.delete('/delete', CRUD.deleteBook);
+
 
 module.exports = router;
