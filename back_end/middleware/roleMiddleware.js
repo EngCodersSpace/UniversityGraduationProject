55dbe96f11a11250171f186f73bb4c462b7e5f4a
Ahmedalgbri12 <151460573+Ahmedalgbri12@users.noreply.github.com>
2025-01-28 22:04:08 +0300

    All assignment operations completed

--- back_end/middleware/roleMiddleware.js
+++ back_end/middleware/roleMiddleware.js
@@ -1,12 +1,12 @@
 
 const checkRole = (requiredRoles) => (req, res, next) => {
-    if (!req.user || !req.user.role.roleName) {
+    if (!req.user || !req.user.permission) {
         return res.status(403).json({ error: 'Permission denied or user not authenticated' });
     }
 
     const roles = Array.isArray(requiredRoles) ? requiredRoles : [requiredRoles];
 
-    if (!roles.includes(req.user.role.roleName)) {
+    if (!roles.includes(req.user.permission)) {
         return res.status(403).json({ error: 'You do not have the required permissions' });
     }
 
--- back_end/routes/assignmentRoute.js
+++ back_end/routes/assignmentRoute.js
@@ -10,20 +10,19 @@ router.post('/check-files', CRUD.getFileDetails);
 router.post('/upload-files-assignment-doctor',CRUD.uploadFileForAssignment);
 router.post('/upload-files-assignment-student', CRUD.uploadFilesAttachment);
 
-
-
 router.get('/get-assignments-subject',CRUD.getAssignmentsOfSubject);
 router.get('/get-all-students-assignment', CRUD.getStudentsAndFilesByAssignment);
 router.get('/download-assignment', CRUD.downloadFile);
 
 // router.get('/get-students-assignment-files',checkRole(['lecturer','controller','representative']), CRUD.getStudentFiles);
 
-router.put('/update-assignment',checkRole(['lecturer','controller']), CRUD.updateAssigment);
-router.put('/update-student-assignment-status',checkRole(['lecturer','controller']), CRUD.updateAssignmentStatus);
-router.put('/update-student-assignment-complete',checkRole(['representative','student']), CRUD.updateStudentComplete);
+router.put('/update-assignment', CRUD.updateAssigment);
+router.put('/update-student-assignment-status', CRUD.updateAssignmentStatus);
+router.put('/update-student-assignment-complete', CRUD.updateStudentComplete);
 
-router.delete('/delete-assignment',checkRole(['controller','lecturer']), CRUD.deleteAssignment);
-router.delete('/delete-assignment-files',checkRole(['controller','lecturer']), CRUD.deleteAssigmentFiles);
+router.delete('/delete-assignment', CRUD.deleteAssignment);
+router.delete('/delete-assignment-files', CRUD.deleteAssigmentFiles);
+router.delete('/delete-attachment-files', CRUD.deleteAttachmentFiles);
 
 
 
