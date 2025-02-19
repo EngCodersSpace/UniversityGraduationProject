5262f1bb7c439e0f7b85b49127c3232d01f86ad9
SHEHAB <shshehab02@gmail.com>
2025-02-19 03:14:54 +0300

    clean after merge

--- ibb_university_students_services/lib/app/views/admin_panel/doctor_table_view/doctor_table_view.dart
+++ ibb_university_students_services/lib/app/views/admin_panel/doctor_table_view/doctor_table_view.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:ibb_university_students_services/app/controllers/admin_panel_controllers/dashboard_doctor_table_controller.dart';
-import 'package:ibb_university_students_services/app/views/admin_panel/dashboard_component/heder_of_view_component.dart';
 
 class DoctorTableView extends GetView<DashboardDoctorTableController> {
   const DoctorTableView({super.key});
