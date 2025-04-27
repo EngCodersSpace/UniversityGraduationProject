b002a81024ad9d30154705a0b7f574c4c35ac7c5
SHEHAB <shshehab02@gmail.com>
2025-04-28 00:41:29 +0300

    set up pepper transiction first route

--- ibb_university_students_services/lib/app/controllers/pepper_transactions_controller.dart
+++ ibb_university_students_services/lib/app/controllers/pepper_transactions_controller.dart
@@ -4,4 +4,5 @@ import 'package:get/get.dart';
 class PepperTransactionsController extends GetxController {
 
   RxBool loadingState = false.obs;
+
 }
--- ibb_university_students_services/lib/app/views/pepper_transactions_view/pepper_transactions_phone_view.dart
+++ ibb_university_students_services/lib/app/views/pepper_transactions_view/pepper_transactions_phone_view.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:ibb_university_students_services/app/components/buttons.dart';
 import 'package:ibb_university_students_services/app/controllers/pepper_transactions_controller.dart';
 import 'package:ibb_university_students_services/app/styles/app_colors.dart';
+import 'package:ibb_university_students_services/app/views/pepper_transactions_view/pepper_transactions_view_components/absence_request_template.dart';
 
 
 class PepperTransactionsPhoneView extends GetView<PepperTransactionsController> {
@@ -16,7 +18,13 @@ class PepperTransactionsPhoneView extends GetView<PepperTransactionsController> 
             ? const Center(
           child: CircularProgressIndicator(),
         )
-            : Placeholder(),)
+            : Column(
+          children: [
+            CustomButton(onPress:(){
+              Get.to(AbsenceRequestTemplate());
+            } ,text: "AbsenceRequestTemplate",)
+          ],
+        ),)
     );
   }
 }
