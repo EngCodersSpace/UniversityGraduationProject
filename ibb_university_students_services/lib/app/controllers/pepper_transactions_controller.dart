1d3eaf5176705af16a87a9441d6985583189bfd8
SHEHAB <shshehab02@gmail.com>
2025-05-05 02:51:30 +0300

    setup filter card in transactions

--- ibb_university_students_services/lib/app/controllers/pepper_transactions_controller.dart
+++ ibb_university_students_services/lib/app/controllers/pepper_transactions_controller.dart
@@ -1,9 +1,96 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
+import '../styles/app_colors.dart';
+import '../views/pepper_transactions_view/pepper_transactions_view_components/transaction_filter_card.dart';
 
 
 class PepperTransactionsController extends GetxController {
 
   RxBool loadingState = false.obs;
   List transactions = [1,2];
+  List<String> status = ["all","pending","rejected","accepted","committed"];
+  TextEditingController searchText = TextEditingController();
+  FocusNode searchFocus = FocusNode();
+  RxString selectedSortOption = "title".obs;
+  RxString selectedFilterOption = "all".obs;
+  RxInt sortDirection = 0.obs;
+  final List<int?> showOptions = [0, 1, 2];
+  Map<String, List<String>> sortOptions = {
+    "title": ["A to Z", "Z to A"],
+    "size": ["Smallest", "Largest"],
+    "page": ["Lowest", "Highest"],
+    "date": ["Oldest", "Newest"],
+  };
+  List<Border> borders = [];
 
+
+  @override
+  void onInit() {
+    BorderSide borderSide =
+    BorderSide(color: AppColors.inverseCardColor, width: 1.0);
+    borders = [
+      Border(
+        top: borderSide,
+        right: borderSide,
+        bottom: borderSide,
+      ),
+      Border(
+        top: borderSide,
+        left: borderSide,
+        bottom: borderSide,
+      ),
+    ];
+    super.onInit();
+  }
+
+  void searching(String? val) {}
+
+  void changeSelectedSortOption(String? val) async {
+    if (val == null) return;
+    selectedSortOption.value = val;
+    // switch (val) {
+    //   case "title":
+    //     books.value = Map<int, LibraryFile>.fromEntries(books.entries.toList()
+    //       ..sort((a, b) => (sortDirection.value == 0)
+    //           ? (a.value.title
+    //           ?.toLowerCase()
+    //           .compareTo(b.value.title?.toLowerCase() ?? "") ??
+    //           0)
+    //           : (b.value.title
+    //           ?.toLowerCase()
+    //           .compareTo(a.value.title?.toLowerCase() ?? "") ??
+    //           0)));
+    //     break;
+    //   case "page":
+    //     books.value = Map<int, LibraryFile>.fromEntries(books.entries.toList()
+    //       ..sort((a, b) => (sortDirection.value == 0)
+    //           ? (a.value.numberOfPages?.compareTo(b.value.numberOfPages ?? 0) ??
+    //           0)
+    //           : (b.value.numberOfPages?.compareTo(a.value.numberOfPages ?? 0) ??
+    //           0)));
+    //     break;
+    //   case "size":
+    //     books.value = Map<int, LibraryFile>.fromEntries(books.entries.toList()
+    //       ..sort((a, b) => (sortDirection.value == 0)
+    //           ? (a.value.fileSize?.compareTo(b.value.fileSize ?? 0) ?? 0)
+    //           : (b.value.fileSize?.compareTo(a.value.fileSize ?? 0) ?? 0)));
+    //     break;
+    // }
+  }
+
+  void changeSelectedSortDirection(int? val) async {
+    if (val == null) return;
+    sortDirection.value = val;
+    changeSelectedSortOption(selectedSortOption.value);
+  }
+
+  void changeSelectedFilterOption(String? val) {
+    if (val == null) return;
+    selectedFilterOption.value = val;
+  }
+
+  void filteringIconClick() {
+    Get.dialog(PopUpTransactionFilterCard());
+  }
 }
--- ibb_university_students_services/lib/app/views/pepper_transactions_view/pepper_transactions_phone_view.dart
+++ ibb_university_students_services/lib/app/views/pepper_transactions_view/pepper_transactions_phone_view.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:ibb_university_students_services/app/components/buttons.dart';
 import 'package:ibb_university_students_services/app/controllers/pepper_transactions_controller.dart';
 import 'package:ibb_university_students_services/app/styles/app_colors.dart';
-import 'package:ibb_university_students_services/app/views/pepper_transactions_view/pepper_transactions_view_components/absence_request_template.dart';
-
 import '../../components/custom_text_v2.dart';
+import '../../components/text_field.dart';
 import '../../styles/text_styles.dart';
 
 class PepperTransactionsPhoneView
@@ -24,10 +22,42 @@ class PepperTransactionsPhoneView
                 )
               : SafeArea(
                 child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      CustomText(
-                          "Academic Transactions".tr,
-                          style: AppTextStyles.secStyle(textHeader: AppTextHeaders.h3Bold)
+                      Row(
+                        children: [
+                          IconButton(
+                              onPressed: () => Get.back(),
+                              icon: Icon(
+                                Icons.arrow_back_outlined,
+                                color: AppColors.inverseCardColor,
+                              )),
+                          CustomText(
+                              "Academic Transactions".tr,
+                              style: AppTextStyles.secStyle(textHeader: AppTextHeaders.h2Bold)
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: CustomTextFormField(
+                              controller: controller.searchText,
+                              focusNode: controller.searchFocus,
+                              onChange: controller.searching,
+                              onTapOutside: (e){controller.searchFocus.unfocus();},
+                              labelText: "Search",
+                              color: AppColors.inverseCardColor,
+                              prefixIcon: Icons.search_rounded,
+                            ),
+                          ),
+                          IconButton(
+                              onPressed: controller.filteringIconClick,
+                              icon: Icon(
+                                Icons.filter_list_alt,
+                                color: AppColors.inverseCardColor,
+                              )),
+                        ],
                       ),
                       Expanded(
                         child: ListView.builder(
